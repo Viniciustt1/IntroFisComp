a4052c8cab91a345305c843782e6bb1e958c0ec0
Vinicius <viniciusyy@usp.br>
2022-10-04 11:16:52 -0300

    modified:   exer05.f90

--- exer05.f90
+++ exer05.f90
@@ -1,7 +1,44 @@
+contains   
+module function
+    implicit none
+    function esc_prod(x, escalar, n) !função que cálcula o produto escalar de dois vetores
+
+    real, dimension(n,1), intent(in) :: x, y
+    real :: escalar 
+    integer :: i, n 
+    
+    escalar = 0
+
+    do i = 1, n 
+     escalar = escalar + x(n,1)*y(n,i) !cálculo do produto escalar
+    end do
+ 
+    end function
+
+    function prod(A, B) !função para a multiplicação de matrizes
+    real, dimension(n,n), intent(in) :: A
+    real, dimension(n,1) :: prod, B
+    integer :: i, j
+    
+   
+    do i = 1, n 
+      do j = 1, n 
+       prod(i,1) = 0
+       prod(i,1) = prod(i,1) + A(i,j)*B(j,1)
+      end do
+    end do
+  
+    end function
+
+end module
+      
+
 program exer05
     IMPLICIT NONE
-    real(4) :: lbd, e !(erro)
-    real(4), allocatable, dimension(:,:) :: M, x, u
+    real :: lbd, e !(erro)
+    integer, parameter :: dp = kind(0.d0)
+    real(kind = dp), allocatable, dimension(:,:) :: M
+    real, dimension(:,:), allocatable :: x, u
     integer :: i, j, n
     
     print*, "Entre com o erro: \e"
@@ -16,16 +53,16 @@ program exer05
       read(*,*) (M(i,j), j = 1, n) !escrevendo as linhas da matriz
     end do
    
-    do i = 1, n !gerando os vetores x e u 
-      x(i, 1) = 1
-      u(i, 1) = 1
+    do i = 1, n !gerando os vetores de teste x e u 
+      x(i, 1) = i
+      u(i, 1) = i+1
     end do
 
     do
       u = prod(M, x) !definindo u como o produto da matriz M com a matriz x
 
-      if (abs(lbd - esc_prod(x, u, 3))/esc_prod(x, x, 3) > e) then
-        lbd = esc_prod(x, u, 3)/esc_prod(x, x, 3)
+      if (abs(lbd - esc_prod(x, u, n))/esc_prod(x, x, n) > e) then
+        lbd = esc_prod(x, u, n)/esc_prod(x, x, n)
         x = u
       else
         exit
@@ -33,42 +70,6 @@ program exer05
 
     print*, lbd
     print*, x
-    
-
-
-
-    contains   
-      function esc_prod(x, escalar, n) !função que cálcula o produto escalar de dois vetores
-
-      implicit none
-      real(4), dimension(n,1), intent(in) :: x, y
-      real(4) :: escalar 
-      integer :: i, n 
-    
-      escalar = 0
-
-      do i = 1, n 
-        escalar = escalar + x(n,1)*y(n,i) !cálculo do produto escalar
-      end do
-      modulo = aux
-    
-      end function
-
-      function prod(A, B) !função para a multiplicação de matrizes
-      real(4), dimension(n,n), intent(in) :: A
-      real(4), dimension(n,1) :: prod, B
-      integer :: i, j
-    
-   
-      do i = 1, n 
-        do j = 1, n 
-        prod(i,1) = 0
-        prod(i,1) = prod(i,1) + A(i,j)*B(j,1)
-        end do
-      end do
-  
-      end function
-      
 
 end program
 
