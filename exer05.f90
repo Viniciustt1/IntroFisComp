df5e8b616639ff7412b4bb51ea7530450fed9ac9
Vinicius <viniciusyy@usp.br>
2022-10-06 19:13:51 -0300

    modified:   exer05.f90

--- exer05.f90
+++ exer05.f90
@@ -1,39 +1,37 @@
-contains   
 module function
-    implicit none
-    function esc_prod(x, escalar, n) !função que cálcula o produto escalar de dois vetores
-
-    real, dimension(n,1), intent(in) :: x, y
-    real :: escalar 
-    integer :: i, n 
+implicit none
+contains   
+function escalar(x, y, n) !função que cálcula o produto escalar de dois vetores
+real, dimension(n,1), intent(in) :: x, y
+real :: escalar 
+integer :: i, n 
     
-    escalar = 0
+escalar = 0
 
-    do i = 1, n 
-     escalar = escalar + x(n,1)*y(n,i) !cálculo do produto escalar
-    end do
+do i = 1, n 
+escalar = escalar + x(n,1)*y(n,i) !cálculo do produto escalar
+end do
  
-    end function
+end function
 
-    function prod(A, B) !função para a multiplicação de matrizes
-    real, dimension(n,n), intent(in) :: A
-    real, dimension(n,1) :: prod, B
-    integer :: i, j
+function prod(A, B) !função para a multiplicação de matrizes
+real, dimension(n,n), intent(in) :: A
+real, dimension(n,1) :: prod, B
+integer :: i, j
     
    
-    do i = 1, n 
-      do j = 1, n 
-       prod(i,1) = 0
-       prod(i,1) = prod(i,1) + A(i,j)*B(j,1)
-      end do
-    end do
+do i = 1, n 
+do j = 1, n 
+prod(i,1) = 0
+prod(i,1) = prod(i,1) + A(i,j)*B(j,1)
+end do
+end do
   
-    end function
-
-end module
-      
+end function
+end module function      
 
 program exer05
+    use function
     IMPLICIT NONE
     real :: lbd, e !(erro)
     integer, parameter :: dp = kind(0.d0)
@@ -55,14 +53,14 @@ program exer05
    
     do i = 1, n !gerando os vetores de teste x e u 
       x(i, 1) = i
-      u(i, 1) = i+1
+      u(i, 1) = i
     end do
 
     do
       u = prod(M, x) !definindo u como o produto da matriz M com a matriz x
 
-      if (abs(lbd - esc_prod(x, u, n))/esc_prod(x, x, n) > e) then
-        lbd = esc_prod(x, u, n)/esc_prod(x, x, n)
+      if (abs(lbd - escalar(x, u, n))/escalar(x, x, n) > e) then
+        lbd = escalar(x, u, n)/escalar(x, x, n)
         x = u
       else
         exit
